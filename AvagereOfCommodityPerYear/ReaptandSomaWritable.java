e76e1ce97852f19eeee2c7b5e77ffc90beca3e8b
Micael Daniel Pardim Fone <104154840+MicaelDanielPardim@users.noreply.github.com>
2023-09-05 22:43:45 -0300

    Update ReaptandSomaWritable.java
    Mudando o Writable do exercício 03

--- AvagereOfCommodityPerYear/ReaptandSomaWritable.java
+++ AvagereOfCommodityPerYear/ReaptandSomaWritable.java
@@ -1,35 +1,35 @@
-package Tde.AvagereOfCommodityPerYear;
+ package Tde.AvagereOfCommodityPerYear;
 import org.apache.hadoop.io.WritableComparable;
+
 import java.util.Objects;
+
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-public class ReaptandSomaWritable implements WritableComparable<ReaptandSomaWritable>{
+public class ReaptandSomaWritable implements WritableComparable<ReaptandSomaWritable> {
     private int reapt;
-    private String soma;
-
+    private float custo;
     public ReaptandSomaWritable() {
     }
-    public ReaptandSomaWritable(int reapt, String soma) {
+    public ReaptandSomaWritable(int reapt, long custo) {
         this.reapt = reapt;
-        this.soma = soma;
+        this.custo = custo;
     }
 
     public int getReapt() {
         return reapt;
     }
 
-
     public void setReapt(int reapt) {
         this.reapt = reapt;
     }
 
-    public float getSoma() {
-        return soma;
+    public float getCusto() {
+        return custo;
     }
 
-    public void setSoma(float soma) {
-        this.soma = soma;
+    public void setCusto(Long custo) {
+        this.custo = custo;
     }
 
     @Override
@@ -37,31 +37,33 @@ public class ReaptandSomaWritable implements WritableComparable<ReaptandSomaWrit
         if (this == o) return true;
         if (!(o instanceof ReaptandSomaWritable)) return false;
         ReaptandSomaWritable that = (ReaptandSomaWritable) o;
-        return reapt == that.reapt && Float.compare(that.soma, soma) == 0;
+        return reapt == that.reapt && custo == that.custo;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(reapt, soma);
+        return Objects.hash(reapt, custo);
     }
 
-    @Override
-    public int compareTo(ReaptandSomaWritable o) {
-        return Integer.compare(this.hashCode(),o.hashCode());
-    }
+
 
     @Override
-    public void write(DataOutput dataOutput) throws IOException {
-        dataOutput.writeInt(reapt);
-        dataOutput.writeFloat(soma);
-
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(reapt);
+        out.writeFloat(custo);
 
     }
 
     @Override
-    public void readFields(DataInput dataInput) throws IOException {
-        reapt = dataInput.readInt();
-        soma = dataInput.readFloat();
-
+    public void readFields(DataInput in) throws IOException {
+        reapt = in.readInt();
+        custo = in.readFloat();
     }
-}
+
+    @Override
+     public int compareTo(ReaptandSomaWritable o) {
+            return Integer.compare(this.hashCode(),o.hashCode());
+        }
+    }
+
+
