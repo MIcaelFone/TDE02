01f6c7d401a21dfac3099b2ea7d814e2f941d9b1
Micael Daniel Pardim Fone <104154840+MicaelDanielPardim@users.noreply.github.com>
2023-09-05 22:42:38 -0300

    Update AverageOfCommodityValuesPerYear.java
    Finalizando o exercício 03

--- AvagereOfCommodityPerYear/AverageOfCommodityValuesPerYear.java
+++ AvagereOfCommodityPerYear/AverageOfCommodityValuesPerYear.java
@@ -1,10 +1,7 @@
 package Tde.AvagereOfCommodityPerYear;
-import Tde.TransictionPerYearandFlow.TransictionPerYearandFlow;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -12,32 +9,31 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.log4j.BasicConfigurator;
-
 import java.io.IOException;
-import java.time.Year;
-import java.util.Objects;
-
 public class AverageOfCommodityValuesPerYear {
+    //country_or_area;year;comm_code;commodity;flow;trade_usd;weight_kg;quantity_name;quantity;category
     public static void main(String[] args) throws Exception {
         BasicConfigurator.configure();
         Configuration c = new Configuration();
         String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
 // arquivo de entrada
+        // arquivo de entrada
         Path input = new Path("in/transactions_amostra.csv");
 // arquivo de saida
         Path output = new Path("output/AvagereOfCommodityPerYear");
 // criacao do job e seu nome
         Job j = new Job(c, "AvagereOfCommodityPerYear");
 // Registro de classes
-        j.setJarByClass(AverageOfCommodityValuesPerYear.class);
-        j.setMapperClass(MapForAvangereOfCommodityValuesPerYear.class);
-        j.setReducerClass(ReduceAvangereOfCommodityValuesPerYear.class);
+        j.setJarByClass(AverageOfCommodityValuesPerYear.class); // Classe que tem o main
+        j.setMapperClass(MapForAverageOfCommodityValuesPerYear.class); // Classe do MAP
+        j.setReducerClass(ReduceAvangereOfCommodityValuesPerYear.class); // Classe do Reduce
         j.setCombinerClass(CombinerAverageofCommodityValuesPerYear.class);
-// Tipos de saída
+// Tipos de saída das classes
         j.setMapOutputKeyClass(Text.class);
         j.setMapOutputValueClass(ReaptandSomaWritable.class);
         j.setOutputKeyClass(Text.class);
         j.setOutputValueClass(FloatWritable.class);
+
 
 // Definir tipos de saida
 // Map
@@ -58,28 +54,45 @@ public class AverageOfCommodityValuesPerYear {
      * 4info tipo do valor de saida (IntWritable-->1)
      * a
      */
-    public static class MapForAvangereOfCommodityValuesPerYear extends Mapper<LongWritable, Text, Text, ReaptandSomaWritable> {
-        public void map(LongWritable key, Text value, Context con)
-                throws IOException, InterruptedException {
+    public static class MapForAverageOfCommodityValuesPerYear extends Mapper<LongWritable, Text, Text, ReaptandSomaWritable> {
+        public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException {
             String linha = value.toString();
-            String Year = linha.split(";")[1];
-            String valueOfCommodity = linha.split(";")[7];
-            con.write(new Text(Year), new ReaptandSomaWritable(1, Float.parseFloat(valueOfCommodity)));
+            String[] parts = linha.split(";");
 
+            String Year = parts[1];
+            String price = parts[5];
+            if (parts.length < 6) {
+                // The line does not have enough fields, skip it
+                return;
+            }
 
+            if (!price.equals("trade_usd")) {
+                try {
+                    long preco = Long.parseLong(price);
+                    con.write(new Text(Year), new ReaptandSomaWritable(1, preco));
+                } catch (NumberFormatException e) {
+                    // Handle the case where price cannot be parsed as a float
+                    // You may log this error or handle it as needed
+                }
+            }
         }
+
+
+
     }
 
+
     public static class CombinerAverageofCommodityValuesPerYear extends Reducer<Text, ReaptandSomaWritable, Text, ReaptandSomaWritable> {
-        public void reduce(Text key, Iterable<ReaptandSomaWritable> values, Context con)
+           public void reduce(Text key, Iterable<ReaptandSomaWritable> values, Context con)
                 throws IOException, InterruptedException {
-            float soma_commidittes = 0.0f;
             int soma_repeat = 0;
+            long soma_valores = 0;
             for (ReaptandSomaWritable v : values) {
-                soma_commidittes += v.getSoma();
                 soma_repeat += v.getReapt();
+                soma_valores += v.getCusto();
+
             }
-            con.write(new Text(key), new ReaptandSomaWritable(soma_repeat, soma_commidittes));
+            con.write(key, new ReaptandSomaWritable(soma_repeat,soma_valores));
 
         }
     }
@@ -93,19 +106,23 @@ public class AverageOfCommodityValuesPerYear {
      * 4info tipo do valor de saida (IntWritable-->1)
      * a
      */
-    public static class ReduceAvangereOfCommodityValuesPerYear extends Reducer<Text, ReaptandSomaWritable, Text, FloatWritable> {
-        public void reduce(Text key, Iterable<ReaptandSomaWritable> values, Context con)
-                    throws IOException, InterruptedException {
-                float soma_commidittes = 0.0f;
-                int soma_repeat = 0;
-                for (ReaptandSomaWritable v : values) {
-                    soma_commidittes += v.getSoma();
-                    soma_repeat += v.getReapt();
-                }
-                float media = soma_commidittes / soma_repeat;
-                con.write(key, new FloatWritable(media));
 
+    public static class ReduceAvangereOfCommodityValuesPerYear extends Reducer<Text, ReaptandSomaWritable, Text,LongWritable> {
+        public void reduce(Text key, Iterable<ReaptandSomaWritable> values, Context con)
+                throws IOException, InterruptedException {
+            long soma_valores = 0;
+            int soma_repeat = 0;
+            for (ReaptandSomaWritable v : values) {
+                soma_valores += v.getCusto();
+                soma_repeat += v.getReapt();
             }
+            long media = soma_valores / soma_repeat;
+
+            con.write(key, new LongWritable(media));
         }
     }
+
+}
+
+
 
